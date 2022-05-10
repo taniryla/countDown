09790ca08cacb2d713781576e0693ad0fe95dc67
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-10 04:10:18 +0200

    function added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-function countDown(num) {}
+function countDown(num) {
+  if (num <= 0) {
+    console.log("All done!");
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+}
+
+countDown(5);

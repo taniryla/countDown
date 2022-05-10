60ab330b148c76ed16c69434a46447bb32662ce8
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-10 04:14:04 +0200

    iteration version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-function countDown(num) {
-  if (num <= 0) {
-    console.log("All done!");
-    return;
-  }
-  console.log(num);
-  num--;
-  countDown(num);
-}
+// function countDown(num) {
+//   if (num <= 0) {
+//     console.log("All done!");
+//     return;
+//   }
+//   console.log(num);
+//   num--;
+//   countDown(num);
+// }
 
 countDown(5);
+
+function countDown(num) {
+  for (var i = num; i < 0; i--) {
+    console.log(i);
+  }
+  console.log("All done!");
+}
